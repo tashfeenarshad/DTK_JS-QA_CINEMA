abcc7a22d2b91236d1d0923295a29c83c8e1a2f3
jariful hoque <jarifulhoque@hotmail.co.uk>
2022-09-05 22:43:18 +0100

    Navbar improvements

--- cinema/src/Components/Navbar.jsx
+++ cinema/src/Components/Navbar.jsx
@@ -1,7 +1,9 @@
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
 
 function Navigation() {
   return (
@@ -13,16 +15,27 @@ function Navigation() {
             <Nav.Link href="/listings">Listings</Nav.Link>
             <Nav.Link href="/bookings">Bookings</Nav.Link>
             <Nav.Link href="/newreleases">New Releases</Nav.Link>
-            <Nav.Link href="/classifications">Classifications</Nav.Link>
             <Nav.Link href="/discussion">Discussion</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown title="Information" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/screens">Screens</NavDropdown.Item>
+              <NavDropdown.Item href="/classifications">Classifications</NavDropdown.Item>
+              <NavDropdown.Item href="/placestogo">Places To Go</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              <NavDropdown.Item href="/directions">Directions</NavDropdown.Item>
+              <NavDropdown.Item href="/openingtimes">Opening Times</NavDropdown.Item>
+              <NavDropdown.Item href="/about">About Us</NavDropdown.Item>
+              <NavDropdown.Item href="/contact">Contact Us</NavDropdown.Item>
             </NavDropdown>
           </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
         </Container>
       </Navbar>
       <br />
