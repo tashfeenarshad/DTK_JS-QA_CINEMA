fa49b3997f26359b328f5c4c93e7a3214e477945
jariful hoque <jarifulhoque@hotmail.co.uk>
2022-09-06 14:29:23 +0100

    contact info added

--- cinema/src/Components/Contact.jsx
+++ cinema/src/Components/Contact.jsx
@@ -6,7 +6,53 @@ import Row from 'react-bootstrap/Row';
 function Contact() {
   return (
     <>
+
+      <div>
+                <Row>
+                    <Col style={{ display: 'flex', justifyContent: 'center' }} >
+                        <h1 style={{ fontWeight: 'bold' }} >Contact Us</h1>
+                    </Col>
+                </Row>
+                <br />
+
+                <Row>
+                    <Col lg="3">
+                        <div id="address-info-div">
+                            <h3>Address</h3>
+                            <p>3rd Floor
+                            <br />
+                            International House
+                            <br />
+                            1 St Katherine's Way
+                            <br />
+                            London
+                            <br />
+                            E1W 1UN</p>
+                        </div>
+                    </Col>
+
+                    <Col lg="3">
+                        <div id="contact-info-div">
+                            <h3>Contacts</h3>
+                            <p>Tel: 
+                            <br />
+                            Email
+                            <br />
+                            Website: 
+                            <br />
+                            Twitter: 
+                            <br />
+                            Instagram:</p>
+                        </div>
+                    </Col>
+                </Row>
+                <br />
+            </div>
+
+
     <Form>
+    <h3>Email Us</h3>
+    <br />
     <Row>
         <Col>
           <Form.Label>First Name</Form.Label>
