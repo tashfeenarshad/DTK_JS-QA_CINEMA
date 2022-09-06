8151fbf23f1152699908ab83d8fa3769aa262b1c
jariful hoque <jarifulhoque@hotmail.co.uk>
2022-09-06 16:43:55 +0100

    added parking

--- cinema/src/Components/Directions.jsx
+++ cinema/src/Components/Directions.jsx
@@ -1,3 +1,4 @@
+import Parking from './Images/Parking.jpg';
 import GoogleMap from './GoogleMap';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
@@ -13,8 +14,9 @@ function Directions() {
                 <br/>
                 <div>
                     <h3>Public Transport</h3>
-                    <p>We are within walking distance of Tower Hill Underground, London Bridge Underground and Tower Gateway DLR station</p>
-                    
+                    <p>We are within walking distance of Tower Hill Underground, London Bridge Underground and Tower Gateway DLR station.
+                      Use our interactive map to search for directions as necessary.
+                    </p>
                     <h3>Address</h3>
                        <p>3rd Floor
                        <br />
@@ -31,7 +33,16 @@ function Directions() {
             <Col className="map-info" lg="6">
                 <GoogleMap />
             </Col>
-        </Row>      
-    </div>
+        </Row> <br/>     
+    <Row>
+    <h3>Parking Availability</h3>
+    <br/>
+    <img
+          className="d-block w-100"
+          src={Parking}
+          alt="Parking"/>
+    </Row>
+
+  </div>
   </>);}
   export default Directions;
