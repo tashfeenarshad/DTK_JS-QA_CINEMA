bcd4203acc0cf20e913896e0190265ba7434cd6b
namvarzaidi <namvar.zaidi@gmail.com>
2022-09-07 15:47:05 +0100

    Added mailto function

--- cinema/src/Components/About.jsx
+++ cinema/src/Components/About.jsx
@@ -1,6 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
+
 
 function About() {
     return (
@@ -8,14 +8,14 @@ function About() {
     <h1>About</h1>
     
 
-  <CardGroup>
-      <Card style={{ width: '20rem' }} >
+  
+      <Card style={{ width: '50rem' }} >
         <Card.Img variant="top" src="holder.js/100px180" alt=""/>
         <Card.Body>
           <Card.Title>Damian Poclitar</Card.Title>
           <Card.Text>
             <h6>Contact Details</h6>
-              <a><Button variant="primary">Email</Button></a> 
+              <a href='mailto:' target="_blank" rel="noopener noreferrer"><Button variant="primary">Email</Button></a> 
           </Card.Text>
           <Card.Text>
               <a href='https://www.linkedin.com/in/damianpoclitar/' target="_blank" rel="noopener noreferrer"><Button variant="primary">LinkedIn</Button></a>
@@ -24,13 +24,13 @@ function About() {
         </Card.Body>
       </Card>
 
-      <Card style={{ width: '20rem' }} >
+      <Card style={{ width: '50rem' }} >
         <Card.Img variant="top" src="holder.js/100px180" alt=""/>
         <Card.Body>
           <Card.Title>Jariful Hoque</Card.Title>
           <Card.Text>
             <h6>Contact Details</h6>
-              <a><Button variant="primary">Email</Button></a> 
+              <a href='mailto:' target="_blank" rel="noopener noreferrer"><Button variant="primary">Email</Button></a> 
           </Card.Text>
           <Card.Text>
               <a href='https://www.linkedin.com/in/jarif-hoque/' target="_blank" rel="noopener noreferrer"><Button variant="primary">LinkedIn</Button></a>
@@ -39,13 +39,13 @@ function About() {
         </Card.Body>
       </Card>
 
-      <Card style={{ width: '20rem' }} >
+      <Card style={{ width: '50rem' }} >
         <Card.Img variant="top" src="holder.js/100px180" alt=""/>
         <Card.Body>
           <Card.Title>Kya Edmeade</Card.Title>
           <Card.Text>
             <h6>Contact Details</h6>
-              <a><Button variant="primary">Email</Button></a> 
+              <a href='mailto:' target="_blank" rel="noopener noreferrer"><Button variant="primary">Email</Button></a> 
           </Card.Text>
           <Card.Text>
               <a href='https://www.linkedin.com/in/kya-edmeade/' target="_blank" rel="noopener noreferrer"><Button variant="primary">LinkedIn</Button></a>
@@ -53,16 +53,16 @@ function About() {
           <a href='https://github.com/K-Edmeade' target="_blank" rel="noopener noreferrer"><Button variant="primary">GitHub</Button></a>
         </Card.Body>
       </Card>
-  </CardGroup>
+  
 
-  <CardGroup>
-  <Card style={{ width: '20rem' }} >
+  
+  <Card style={{ width: '50rem' }} >
         <Card.Img variant="top" src="holder.js/100px180" alt=""/>
         <Card.Body>
           <Card.Title>Tashfeen Arshad</Card.Title>
           <Card.Text>
             <h6>Contact Details</h6>
-              <a><Button variant="primary">Email</Button></a> 
+              <a href='mailto:' target="_blank" rel="noopener noreferrer"><Button variant="primary">Email</Button></a> 
           </Card.Text>
           <Card.Text>
               <a href=''target="_blank" rel="noopener noreferrer"><Button variant="primary">LinkedIn</Button></a>
@@ -71,13 +71,13 @@ function About() {
         </Card.Body>
       </Card>
 
-      <Card style={{ width: '20rem' }} >
+      <Card style={{ width: '50rem' }} >
         <Card.Img variant="top" src="holder.js/100px180" alt=""/>
         <Card.Body>
           <Card.Title>Syed Zaidi</Card.Title>
           <Card.Text>
             <h6>Contact Details</h6>
-              <a><Button variant="primary">Email</Button></a> 
+              <a href='mailto:' target="_blank" rel="noopener noreferrer"><Button variant="primary">Email</Button></a> 
           </Card.Text>
           <Card.Text>
               <a href='https://www.linkedin.com/in/syed-namvar-zaidi/' target="_blank" rel="noopener noreferrer"><Button variant="primary">LinkedIn</Button></a>
@@ -86,9 +86,9 @@ function About() {
         </Card.Body>
       </Card>
       
-  </CardGroup>
+  
 
-  <Card style={{ width: '70em' }} >
+  <Card style={{ width: '50em' }} >
         <Card.Img variant="top" src="holder.js/100px180" alt=""/>
         <Card.Body>
           <Card.Title>Scrum</Card.Title>
