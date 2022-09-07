a839ffd7ecdb0d056f73e2ee11e763c0f698b491
namvarzaidi <namvar.zaidi@gmail.com>
2022-09-07 09:36:11 +0100

    Adjusted card size

--- cinema/src/Components/OpeningTimes.jsx
+++ cinema/src/Components/OpeningTimes.jsx
@@ -6,74 +6,74 @@ function OpeningTimes() {
   <>
     <h1>Opening Times</h1>
 
-      <Card style={{ width: '18rem' }} >
+      <Card style={{ width: '20rem' }} >
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
         <Card.Title>Monday</Card.Title>
         <Card.Text>
           10:00 - 23:00
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary">Booking</Button>
       </Card.Body>
     </Card>
-    <Card style={{ width: '18rem' }}>
+    <Card style={{ width: '20rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
         <Card.Title>Tuesday</Card.Title>
         <Card.Text>
           10:00 - 23:00
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary">Booking</Button>
       </Card.Body>
     </Card>
-    <Card style={{ width: '18rem' }}>
+    <Card style={{ width: '20rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
         <Card.Title>Wednesday</Card.Title>
         <Card.Text>
           10:00 - 23:00
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary">Booking</Button>
       </Card.Body>
     </Card>
-    <Card style={{ width: '18rem' }}>
+    <Card style={{ width: '20rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
         <Card.Title>Thursday</Card.Title>
         <Card.Text>
           10:00 - 23:00
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary">Booking</Button>
       </Card.Body>
     </Card>
-    <Card style={{ width: '18rem' }}>
+    <Card style={{ width: '20rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
         <Card.Title>Friday</Card.Title>
         <Card.Text>
           10:00 - 00:00
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary">Booking</Button>
       </Card.Body>
     </Card>
-    <Card style={{ width: '18rem' }}>
+    <Card style={{ width: '20rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
         <Card.Title>Saturday</Card.Title>
         <Card.Text>
           10:00 - 00:00
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary">Booking</Button>
       </Card.Body>
     </Card>
-    <Card style={{ width: '18rem' }}>
+    <Card style={{ width: '20rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
         <Card.Title>Sunday</Card.Title>
         <Card.Text>
           10:00 - 23:00
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary">Booking</Button>
       </Card.Body>
     </Card>
   </>  );
