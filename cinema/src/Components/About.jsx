1001402cf0442dde52b6d975514f292a62f78957
namvarzaidi <namvar.zaidi@gmail.com>
2022-09-07 15:08:28 +0100

    Created about cards, added linkedin and github

--- cinema/src/Components/About.jsx
+++ cinema/src/Components/About.jsx
@@ -1,3 +1,93 @@
-export default function About() {
-    return <h1>About</h1>
-  }
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+
+function About() {
+    return (
+  <>
+    <h1>About</h1>
+    
+
+  <CardGroup>
+      <Card style={{ width: '20rem' }} >
+        <Card.Img variant="top" src="holder.js/100px180" alt=""/>
+        <Card.Body>
+          <Card.Title>Damian Poclitar</Card.Title>
+          <Card.Text>
+            <h6>Contact Details</h6>
+              <a><Button variant="primary">Email</Button></a> 
+          </Card.Text>
+          <Card.Text>
+              <a href='https://www.linkedin.com/in/damianpoclitar/' target="_blank" rel="noopener noreferrer"><Button variant="primary">LinkedIn</Button></a>
+          </Card.Text>
+          <a href='https://github.com/damipoc' target="_blank" rel="noopener noreferrer"><Button variant="primary">GitHub</Button></a>
+        </Card.Body>
+      </Card>
+
+      <Card style={{ width: '20rem' }} >
+        <Card.Img variant="top" src="holder.js/100px180" alt=""/>
+        <Card.Body>
+          <Card.Title>Jariful Hoque</Card.Title>
+          <Card.Text>
+            <h6>Contact Details</h6>
+              <a><Button variant="primary">Email</Button></a> 
+          </Card.Text>
+          <Card.Text>
+              <a href='https://www.linkedin.com/in/jarif-hoque/' target="_blank" rel="noopener noreferrer"><Button variant="primary">LinkedIn</Button></a>
+          </Card.Text>
+          <a href='https://github.com/jarifulhoque' target="_blank" rel="noopener noreferrer"><Button variant="primary">GitHub</Button></a>
+        </Card.Body>
+      </Card>
+
+      <Card style={{ width: '20rem' }} >
+        <Card.Img variant="top" src="holder.js/100px180" alt=""/>
+        <Card.Body>
+          <Card.Title>Kya Edmeade</Card.Title>
+          <Card.Text>
+            <h6>Contact Details</h6>
+              <a><Button variant="primary">Email</Button></a> 
+          </Card.Text>
+          <Card.Text>
+              <a href='https://www.linkedin.com/in/kya-edmeade/' target="_blank" rel="noopener noreferrer"><Button variant="primary">LinkedIn</Button></a>
+          </Card.Text>
+          <a href='https://github.com/K-Edmeade' target="_blank" rel="noopener noreferrer"><Button variant="primary">GitHub</Button></a>
+        </Card.Body>
+      </Card>
+  </CardGroup>
+
+  <CardGroup>
+  <Card style={{ width: '20rem' }} >
+        <Card.Img variant="top" src="holder.js/100px180" alt=""/>
+        <Card.Body>
+          <Card.Title>Tashfeen Arshad</Card.Title>
+          <Card.Text>
+            <h6>Contact Details</h6>
+              <a><Button variant="primary">Email</Button></a> 
+          </Card.Text>
+          <Card.Text>
+              <a href=''target="_blank" rel="noopener noreferrer"><Button variant="primary">LinkedIn</Button></a>
+          </Card.Text>
+          <a href='https://github.com/tashfeenarshad'target="_blank" rel="noopener noreferrer"><Button variant="primary">GitHub</Button></a>
+        </Card.Body>
+      </Card>
+
+      <Card style={{ width: '20rem' }} >
+        <Card.Img variant="top" src="holder.js/100px180" alt=""/>
+        <Card.Body>
+          <Card.Title>Syed Zaidi</Card.Title>
+          <Card.Text>
+            <h6>Contact Details</h6>
+              <a><Button variant="primary">Email</Button></a> 
+          </Card.Text>
+          <Card.Text>
+              <a href='https://www.linkedin.com/in/syed-namvar-zaidi/' target="_blank" rel="noopener noreferrer"><Button variant="primary">LinkedIn</Button></a>
+          </Card.Text>
+          <a href='https://github.com/namvarzaidi' target="_blank" rel="noopener noreferrer"><Button variant="primary">GitHub</Button></a>
+        </Card.Body>
+      </Card>
+      
+  </CardGroup>
+
+
+    </>);}
+  export default About;
