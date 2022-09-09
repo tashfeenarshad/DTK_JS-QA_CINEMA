64358d2a8ce5322fce48a3a9104a8051efa88fb2
namvarzaidi <namvar.zaidi@gmail.com>
2022-09-09 15:01:48 +0100

    Updated scrum methodology

--- cinema/src/Components/About.jsx
+++ cinema/src/Components/About.jsx
@@ -91,11 +91,21 @@ function About() {
   <Card style={{ width: '50em' }} >
         <Card.Img variant="top" src="holder.js/100px180" alt=""/>
         <Card.Body>
-          <Card.Title>Scrum</Card.Title>
+          <Card.Title><center>Scrum</center></Card.Title>
           <Card.Text>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ut illo eaque perferendis sequi recusandae debitis excepturi amet! Harum similique illum veniam ratione eius. Excepturi nesciunt culpa placeat repellendus rem!
+
+          <p>Scrum is a framework for project management that emphasizes teamwork, accountability and iterative progress toward a well-defined goal. The framework begins with a simple premise: Start with what can be seen or known. After that, track the progress and tweak, as necessary.</p>
+
+          <p>Scrum helps people and teams deliver value incrementally in a collaborative manner. Scrums are broken down into a series of iterations known as sprints. 
+              A sprint can last as long as is necessary anywhere between one to four weeks, with each sprint aiming to deliver functioning components of the final product that could be published. </p>
+           
+          <p>Within sprints, user stores are created to track the creation of individual components. User stories are three line stories told from the perspective of the individual which outline the needs. User stories are prioritised so each team member knows where to focus their time.  </p>
+
+          <p>The team made use of scrum methodology, as well a kanban board to create and track the user stories and tasks required for the building of this site. With each componet borken down into an idinviidual task it helped streamline and focus the process, such that components were built in tandem rather than sequentially.</p>
+
+              
           </Card.Text>
-          <a href='' target="_blank" rel="noopener noreferrer"><Button variant="primary">Scrum.org</Button></a>
+          <a href='https://www.scrum.org/' target="_blank" rel="noopener noreferrer"><center><Button variant="primary">Find out more</Button></center></a>
         </Card.Body>
     </Card>
 
