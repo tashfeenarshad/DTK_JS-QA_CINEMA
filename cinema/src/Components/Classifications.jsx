8569baf339eff79aec44672be392233fff819a3a
K-Edmeade <kya.edmeade@gmail.com>
2022-09-12 10:51:42 +0100

    Classificiation styling

--- cinema/src/Components/Classifications.jsx
+++ cinema/src/Components/Classifications.jsx
@@ -5,25 +5,28 @@ import PGClassification from './Images/PGClassification.jpg'
 import TwelveClassification from './Images/12Classification.jpg'
 import FifteenClassification from './Images/15Classification.jpg'
 import EighteenClassification from './Images/18Classification.jpg'
+import Row from 'react-bootstrap/esm/Row';
+
 
 
 export default function Classifications() {
+
+  
     return(
     <>
     <h1>Classifications</h1>
-    <div display ="inline-grid"><Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={UClassification} />
+      <Row><Card style={{ width: '18rem' }} className="classification-top-row"><Card.Img variant="top" src={UClassification} />
       <Card.Body>
         <Card.Title>U Classification</Card.Title>
         <Card.Text>
         A U film should be suitable for audiences aged four years and over, 
         although it is impossible to predict what might upset any particular child.
         </Card.Text>
-        <a href ="https://www.bbfc.co.uk/rating/u" target="_blank" rel="noreferrer"> <Button variant="success" >Read More</Button></a>
+        <a href ="https://www.bbfc.co.uk/rating/u" target="_blank" rel="noreferrer"> <Button variant="success" className='Buttons' >Read More</Button></a>
        
       </Card.Body>
     </Card>
-    <Card style={{ width: '18rem' }}>
+    <Card style={{ width: '18rem' }} className="classification-top-row">
       <Card.Img variant="top" src={PGClassification} />
       <Card.Body>
         <Card.Title>PG Classification</Card.Title>
@@ -33,10 +36,10 @@ export default function Classifications() {
         Unaccompanied children of any age may watch, but parents are advised 
         to consider whether the content may upset younger, or more sensitive, children.
         </Card.Text>
-        <a href ="https://www.bbfc.co.uk/rating/pg" target="_blank" rel="noreferrer"><Button variant="warning">Read More</Button></a>
+        <a href ="https://www.bbfc.co.uk/rating/pg" target="_blank" rel="noreferrer"><Button variant="warning" className='Buttons'>Read More</Button></a>
       </Card.Body>
     </Card>
-    <Card style={{ width: '18rem' }}>
+    <Card style={{ width: '18rem' }} className="classification-top-row">
       <Card.Img variant="top" src={TwelveClassification} />
       <Card.Body>
         <Card.Title>12 Classification</Card.Title>
@@ -49,7 +52,8 @@ export default function Classifications() {
         <a href ="https://www.bbfc.co.uk/rating/12" target="_blank" rel="noreferrer"><Button variant="warning">Go somewhere</Button></a>
     
       </Card.Body>
-    </Card></div>
+    </Card>
+      
     
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={FifteenClassification} />
@@ -69,10 +73,11 @@ export default function Classifications() {
         <Card.Text>
         No one younger than 18 may see an 18 film in a cinema. 
         No one younger than 18 may rent or buy an 18 rated video work. 
-        Adults should be free to choose their own entertainment.
+        
         </Card.Text>
         <a href ="https://www.bbfc.co.uk/rating/18" target="_blank" rel="noreferrer"><Button variant="danger">Read More</Button></a>
       </Card.Body>
-    </Card>
+    </Card></Row>
+       
     </>) 
   }
