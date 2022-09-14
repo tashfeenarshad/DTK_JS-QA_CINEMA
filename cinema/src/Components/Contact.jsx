068ab64400393e44e34c8ff3e934616dfab2b6bb
damipoc <damian7DP@hotmail.com>
2022-09-14 15:36:48 +0100

    Adjusted contact us styling

--- cinema/src/Components/Contact.jsx
+++ cinema/src/Components/Contact.jsx
@@ -23,83 +23,75 @@ export const Contact = () => {
 
   return (
     <>
+      <br />
+      <br />
+      <h1 style={{ display: 'flex', justifyContent: 'center', fontWeight: 'bold' }}>Contact Us</h1>
 
-      <div>
-        <Row>
-          <Col style={{ display: 'flex', justifyContent: 'center' }} >
-            <h1 style={{ fontWeight: 'bold' }} >Contact Us</h1>
-          </Col>
-        </Row>
-        <br />
 
-        <Row>
-          <Col lg="3">
-            <div id="address-info-div">
-              <h3>Address</h3>
-              <p>3rd Floor
-                <br />
-                International House
-                <br />
-                1 St Katherine's Way
-                <br />
-                London
-                <br />
-                E1W 1UN</p>
-            </div>
-          </Col>
+      <div className='contactContainer'>
+        <div className='contactAddressContainer'>
+          <h3>Address</h3>
+          <p>3rd Floor
+            <br />
+            International House
+            <br />
+            1 St Katherine's Way
+            <br />
+            London
+            <br />
+            E1W 1UN</p>
+            <br/>
 
-          <Col lg="3">
-            <div id="contact-info-div">
-              <h3>Contacts</h3>
-              <p>Tel:
-                <br />
-                Email
-                <br />
-                Website:
-                <br />
-                Twitter:
-                <br />
-                Instagram:</p>
-            </div>
-          </Col>
-        </Row>
-        <br />
+          <h3>Contacts</h3>
+          <p>Tel:
+            <br />
+            Email
+            <br />
+            Website:
+            <br />
+            Twitter:
+            <br />
+            Instagram:</p>
+        </div>
+
+
+
+        <div className='contactEmailContainer'>
+          <Form ref={form} onSubmit={sendEmail}>
+            <h3 style={{ display: 'flex', justifyContent: 'center', fontWeight: 'bold' }}>Email Us</h3>
+            <Row classNAme="mb-3">
+              <Form.Group as={Col} controlId="formGridText">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control type="text" name="first_name" placeholder="John" required />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridText">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control name="last_name" placeholder="Smith" required />
+              </Form.Group>
+            </Row>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control name="email" type="email" placeholder="name@example.com" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Subject</Form.Label>
+              <Form.Control type="text" placeholder="Enter the subject of your email" required />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Message</Form.Label>
+              <Form.Control name="message" as="textarea" rows={3} placeholder="Enter your message" required />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" >
+              Submit
+            </Button>
+          </Form>
+        </div>
       </div>
-
-
-      <Form ref={form} onSubmit={sendEmail}>
-        <h3>Email Us</h3>
-        <br />
-        <Row>
-          <Col>
-            <Form.Label>First Name</Form.Label>
-            <Form.Control type="text" name="first_name" placeholder="John" required />
-          </Col>
-          <Col>
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control name="last_name" placeholder="Smith" required />
-          </Col>
-        </Row>
-        <br />
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control name="email" type="email" placeholder="name@example.com" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Subject</Form.Label>
-          <Form.Control name="subject" as="textarea" placeholder="Enter the subject of your email" required />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Message</Form.Label>
-          <Form.Control name="message" as="textarea" rows={3} placeholder="Enter your message" required />
-        </Form.Group>
-
-        <Button variant="primary" type="submit" >
-          Submit
-        </Button>
-      </Form>
     </>
   );
 }
